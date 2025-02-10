6147896a5f880c988fd41d29f7199bd86dd41944
sidz111 <sssurwade2212@gmail.com>
2025-02-10 21:57:42 +0530

    BCrypt 1st commit from siddhu

---
+++ src/main/java/com/bcrypt/BCryptBankApplication.java
@@ -0,0 +1,13 @@
+package com.bcrypt;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BCryptBankApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BCryptBankApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/bcrypt/BCryptBankApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bcrypt;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BCryptBankApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
