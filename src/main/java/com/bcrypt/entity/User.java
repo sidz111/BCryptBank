f171051979c5235516b85d5bfa6e334f173a5e81
Deepesh Prajapati <prajapatideepesh055@gmail.com>
2025-02-12 21:29:30 +0530

    created user-entity class by deepesh

--- src/main/java/com/bcrypt/entity/User.java
+++ src/main/java/com/bcrypt/entity/User.java
@@ -1,6 +1,103 @@
 package com.bcrypt.entity;
 
-public class User {
-            // from akash's side.
-			// from deepesh's side.
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name="user_details")
+public class User 
+{
+	@Id
+	@GeneratedValue(strategy=GenerationType.IDENTITY)
+	@Column(name="user_id")
+	private Integer id;
+	
+	@Column(name="user_name", nullable=false)
+	private String name;
+	
+	@Column(name="user_username", unique=true, nullable=false)
+	private String username;
+	
+	@Column(name="user_password", unique=true, nullable=false)
+	private String password;
+	
+	@Column(name="user_email", unique=true, nullable=false)
+	private String email;
+	
+	@Column(name="user_city", nullable=false)
+	private String city;
+	
+	private String role;
+	
+	@Column(name="user_accountno", unique=true)
+	private Long accountno;
+	
+	public Long getAccountno() {
+		return accountno;
+	}
+
+	public void setAccountno(Long accountno) {
+		this.accountno = accountno;
+	}
+
+	public String getRole() {
+		return role;
+	}
+
+	public void setRole(String role) {
+		this.role = role;
+	}
+
+	public Integer getId() {
+		return id;
+	}
+
+	public void setId(Integer id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getUsername() {
+		return username;
+	}
+
+	public void setUsername(String username) {
+		this.username = username;
+	}
+
+	public String getPassword() {
+		return password;
+	}
+
+	public void setPassword(String password) {
+		this.password = password;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public String getCity() {
+		return city;
+	}
+
+	public void setCity(String city) {
+		this.city = city;
+	}
+	
 }
