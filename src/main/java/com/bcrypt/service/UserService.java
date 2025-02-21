a52fec3d7917ffdfbd693d3ed855fdfcb723ddd1
Deepesh Prajapati <prajapatideepesh055@gmail.com>
2025-02-21 23:16:21 +0530

    UserService Done :- Deepesh Prajapati

--- src/main/java/com/bcrypt/service/UserService.java
+++ src/main/java/com/bcrypt/service/UserService.java
@@ -1,24 +1,49 @@
 package com.bcrypt.service;
 
+import java.util.List;
+import java.util.Optional;
+
 import com.bcrypt.entity.User;
 
 public interface UserService {
 	
 	//Add User
 	User addUser(User user);
-
-	//Update User
-	//Delete User
-	//get user by id;
-	//Get User By username
-	//get User by email
-	//get User by account no
-	//get user by addhar
-	//get user list by Branch
-	//get all user list
-	// is user exist by username
-	// is user exist by email
-	// is user exist by adhar
 	
+	//Update User
+	User updateUser(User user);
+	
+	//Delete User
+	void deleteUser(Integer id);
+	
+	//get user by id;
+	Optional<User> getUserById(Integer id);
+	
+	//Get User By username
+	Optional<User> getUserByUsername(String username);
+	
+	//get User by email
+	Optional<User> getUserByEmail(String email);
+	
+	//get User by account no
+	Optional<User> getUserByAccountNo(Long accountNo);
+	
+	//get user by addhar
+	Optional<User> getUserByAdharNo(Long adhar);
+	
+	//get user list by Branch
+	List<User> getUserByBranch(String branch);
+	
+	//get all user list
+	List<User> getAllUsers();
+	
+	// is user exist by username
+	boolean existsUserByUsername(String username);
+	
+	// is user exist by email
+	boolean existsUserByEmail(String email);
+	
+	// is user exist by adhar
+	boolean existsUserByAadhar(Long adhar);
 	
 }
