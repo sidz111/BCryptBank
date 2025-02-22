5d5e8ba23d693692f50d02c831fecda6526337b4
Deepesh Prajapati <prajapatideepesh055@gmail.com>
2025-02-22 23:18:40 +0530

    I did some changes related to UI : Deepesh

--- src/main/resources/static/js/script.js
+++ src/main/resources/static/js/script.js
@@ -10,6 +10,35 @@ document.getElementById("togglePassword").addEventListener("click", function() {
 	}
 });
 
+document.addEventListener("DOMContentLoaded", function() {
+	document.getElementById("readMoreBtn").addEventListener("click", function() {
+		var moreText = document.getElementById("moreText");
+		var btnText = document.getElementById("readMoreBtn");
+
+		if (moreText.classList.contains("d-none")) {
+			moreText.classList.remove("d-none");
+			btnText.innerHTML = "Read Less";
+		} else {
+			moreText.classList.add("d-none");
+			btnText.innerHTML = "Read More";
+		}
+	});
+});
+
+function toggleSubmenu(event, submenuId) {
+	event.preventDefault();
+	let submenu = document.getElementById(submenuId);
+	let icon = event.target.querySelector("i");
+
+	if (submenu.classList.contains("show")) {
+		submenu.classList.remove("show");
+		icon.classList.replace("fa-minus", "fa-plus");
+	} else {
+		submenu.classList.add("show");
+		icon.classList.replace("fa-plus", "fa-minus");
+	}
+}
+
 /*This is not working, i will work on it.*/
 /*document.addEventListener("DOMContentLoaded", function() {
 	document.getElementById("readMoreBtn").addEventListener("click", function() {
