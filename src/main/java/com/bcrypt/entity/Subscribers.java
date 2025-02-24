facbf95cdb5241daaf94c46a838c92b3577d6f46
sidz111 <sssurwade2212@gmail.com>
2025-02-24 23:10:23 +0530

    Added Subscribers module by Siddhu

---
+++ src/main/java/com/bcrypt/entity/Subscribers.java
@@ -0,0 +1,32 @@
+package com.bcrypt.entity;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+@Entity
+public class Subscribers {
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+	
+	private String email;
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+}
